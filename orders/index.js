ca8ae3e04b590a6dc56386975b56edd89dece62a
Luiz Calaça <luizcalaca@gmail.com>
2022-05-26 16:21:54 -0300

    fix: fixing ports into Docker and from API

--- orders/index.js
+++ orders/index.js
@@ -2,4 +2,4 @@ const app = require('express')();
 
 app.get('/orders', (req, res) => res.send('Hello Orders, API!'));
 
-app.listen(3002, () => console.log(`Products API listening on port 3002!`));
+app.listen(3000, () => console.log(`Products API listening on port 3000!`));
