cff2036b1414e4ad7a1543e3945e0a7a9c181bfb
Luiz Calaça <luizcalaca@gmail.com>
2022-05-26 16:29:41 -0300

    fix: ports on Docker and API also including the npm start script

--- products/index.js
+++ products/index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
 
-app.get('/orders', (req, res) => res.send('Hello Products, API with MySQL!'));
+app.get('/products', (req, res) => res.send('Hello Products, API with MySQL!'));
 
-app.listen(3003, () => console.log(`Products API listening on port 3003!`));
+app.listen(3000, () => console.log(`Products API listening on port 3000!`));
